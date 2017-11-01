849daef56a1cfd21c07c945da47b53f867649df1
Yarik Genza <gsmeter@gmail.com>
2017-11-01 23:22:53 +0200

    integrate native-base theme-provider

--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
-import { View, Text } from 'native-base';
+import { Container, Content, Text, StyleProvider } from 'native-base';
+import getTheme from './theme/components';
 
 class App extends Component {
   render = () => (
-    <View style={styles.container}>
-      <Text style={styles.container}>
-        Hello world!
-        (U-night app)
-      </Text>
-    </View>
+    <StyleProvider style={getTheme()}>
+          <Container>
+            <Content>
+              <Text>
+                Hello world!
+                (U-night app)
+              </Text>
+            </Content>
+          </Container>
+        </StyleProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  }
-});
 
 export { App }; 
