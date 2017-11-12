49861bde8ce41eff298d2932d72419b8c06e8849
Yarik Genza <gsmeter@gmail.com>
2017-11-12 17:23:10 +0200

    add basic card style

--- src/components/event/EventCard.js
+++ src/components/event/EventCard.js
@@ -15,21 +15,28 @@ class EventCard extends Component {
           </Body>
         </Left>
         <Right>
-        
+            <Text style={{ fontSize: 20 }}>17 Aug</Text>
+            <Text style={{ color: '#9e4ed4'}}>Monday</Text>
         </Right>
       </CardItem>
       <CardItem cardBody>
-        
           <Image source={{uri: 'https://scontent-waw1-1.xx.fbcdn.net/v/t35.0-12/23516240_1967565000177123_222602682_o.jpg?oh=4dfeb504d2a578ff8721cbeaf72634e6&oe=5A090654'}} style={{height: 200, width: 350, flex: 1}}/>
-        
       </CardItem>
       <CardItem>
         <Left>
-          <Button transparent textStyle={{color: '#87838B'}}>
-            <Icon name="logo-github" />
-            <Text>1,926 stars</Text>
-          </Button>
+          <Text style={{
+            fontSize: 24,
+            fontWeight: 'bold',
+          }}>MONDAY PARTY</Text>
         </Left>
+        <Right>
+          <Button transparent>
+            <Icon name="clock" style={{ fontSize: 35, color: 'black'}}/>
+            <Text style={{ fontSize: 30, color: 'black',
+          lineHeight: 35, fontWeight: '300',
+          }}>22:00</Text>
+          </Button>
+        </Right>
       </CardItem>
     </Card>
     );
