aa4bdbbf6c3eb1ef059138512273ae6b293579ab
Yarik Genza <gsmeter@gmail.com>
2017-11-13 17:15:18 +0200

    small fix

--- src/components/event/EventCard.js
+++ src/components/event/EventCard.js
@@ -5,46 +5,35 @@ import { Card, CardItem, Thumbnail, Text, Button, Icon, Left, Right, Body } from
 class EventCard extends Component {
   render () {
     return ( 
-      <Card style={{flex: 0, backgroundColor: 'black'}}>
+      <Card style={{ flex: 0, backgroundColor: 'black' }}>
       <CardItem>
         <Left>
-          <Thumbnail source={{uri: 'http://lviv-online.com/ua/wp-content/uploads/2016/09/malevich-club-lviv-logo.jpg'}} />
-          <Body style={styles.mainContaiener}>
-              <View style={styles.clubInfo}>
-                <Text style={styles.clubName}>Malevich club</Text>
-                <Text style={styles.clubRating}>Rating:4</Text>
-              </View> 
-             
-              <View style={styles.date}>
-                <Text style={styles.dateNum}>23</Text>
-                <Text style={styles.dateMonth}>Aug</Text>
-              </View>
-              <View style={styles.day}>
-                <Text style={styles.dateDay}>monday</Text>
-              </View>   
-          </Body>
-        </Left>
+           <Thumbnail source={{uri: 'http://lviv-online.com/ua/wp-content/uploads/2016/09/malevich-club-lviv-logo.jpg'}} />
+           <Body>
+             <Text>Malevich club </Text>
+             <Text note>Rating: 4.5</Text>
+           </Body>
+         </Left>
+         <Right>
+           <Text style={styles.date}>23 Aug</Text>
+           <View style={styles.day}>
+            <Text style={styles.dateDay}>monday</Text>
+          </View>
+         </Right>
       </CardItem>
       <CardItem cardBody>
-        
-          <Image source={{uri: 'https://scontent-waw1-1.xx.fbcdn.net/v/t35.0-12/23516240_1967565000177123_222602682_o.jpg?oh=4dfeb504d2a578ff8721cbeaf72634e6&oe=5A090654'}} style={{height: 200, width: 350, flex: 1}}/>
-        
+        <Image source={{uri: 'https://scontent.flwo1-1.fna.fbcdn.net/v/t35.0-12/23516209_1967578123509144_294036789_o.jpg?oh=024b55ac16e78119d6f9b3b4a4b46c44&oe=5A0B9CDA'}} style={styles.bodyImage}/>
       </CardItem>
-      <CardItem>
-        <Left style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}> 
-          <Button transparent>
-            <Text style={styles.textButton}>MONDAY PARTY</Text>
-          </Button> 
+      <CardItem style={{flex: 1}}>
+        <Text style={styles.nameText}>MONDAY PARTY</Text>
+        <View style={{marginLeft: 'auto'}}>
           <Text style={styles.time}>
-            22
-          <Text style={styles.doubleDot}>
-            :
-          <Text style ={styles.time}>
-            22
-        </Text>
-        </Text>
-      </Text>
-        </Left>
+            09
+            <Text style={styles.doubleDot}>:
+              <Text style={styles.time}>00</Text>
+            </Text>
+          </Text>
+        </View>
       </CardItem>
     </Card>
     );
@@ -56,48 +45,29 @@ const styles = StyleSheet.create ({
     flex: 1,  
     justifyContent: 'space-between',
   },
+  bodyImage: {
+    height: 200,
+    width: 350,
+    flex: 1,
+  },
   date: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    marginTop: -50,
-  },
-  day: {
-    alignItems: 'flex-end',
-  },
-  dateMonth: {
-    fontSize: 20,
-    fontWeight: '500',
-
-    paddingLeft: 5,
+    fontSize: 21,
   },
   dateDay: {
     color: '#800080',
   },
-  dateNum: {
-    fontWeight: '500',
-    fontSize: 20,
-  },
-  clubName: {
-    fontWeight: '500',
-    fontSize: 20,
-  },
-  clubRating: {
-    opacity: 0.5,
-  },
-  textButton: {
+  nameText: {
     fontWeight: '600',
-    fontSize: 22,
-    
+    fontSize: 21,
     color: '#000000',
   },
   time: {
     fontSize: 24,
     fontWeight:'500',
     lineHeight: 30,
-    marginRight: 15,
     borderLeftWidth: 4,
     borderLeftColor: '#800080',
-    paddingLeft: 10,
+    paddingLeft: 10
   },
   doubleDot: {
     fontSize: 25,
