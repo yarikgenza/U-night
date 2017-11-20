4c8ecb1261df06c884ececbbbb41d4c28dca5a1c
Yarik Genza <gsmeter@gmail.com>
2017-11-20 12:13:45 +0200

    get events from back-end

--- src/components/event/EventCard.js
+++ src/components/event/EventCard.js
@@ -1,36 +1,79 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import { Image,  StyleSheet, View } from 'react-native';
 import { Card, CardItem, Thumbnail, Text, Button, Icon, Left, Right, Body } from 'native-base';
 
+
+  // Default values. Replace with provided by api when api ready.
+const DEFAULT_VALUES = {
+  club: {
+    name: 'Malevich club',
+    logoUrl: 'http://lviv-online.com/ua/wp-content/uploads/2016/09/malevich-club-lviv-logo.jpg',
+    rating: 4.5,
+  },
+  event: {
+    photoUrl: 'https://scontent.flwo1-1.fna.fbcdn.net/v/t35.0-12/23516209_1967578123509144_294036789_o.jpg?oh=024b55ac16e78119d6f9b3b4a4b46c44&oe=5A0B9CDA',
+  }
+}
+
+
 class EventCard extends Component {
   render () {
+
+    const { data } = this.props;
+
+    const getDayNumber = (date) => {
+      const dow = moment(date);
+      return dow.format('D')
+    }
+
+    const getMonthText = (date) => {
+      const dow = moment(date);
+      return dow.format('MMM');
+    }
+
+    const getWeekDayText = (date) => {
+      const dow = moment(date);
+      return dow.format('dddd');
+    }
+
+    const getDateHour = (date) => {
+      const dow = moment(date);
+      return dow.format('H');
+    }
+
+    const getDateMinute = (date) => {
+      const dow = moment(date);
+      return dow.format('m');
+    }
+
     return ( 
       <Card style={{ flex: 0, backgroundColor: 'black' }}>
       <CardItem>
         <Left>
-           <Thumbnail source={{uri: 'http://lviv-online.com/ua/wp-content/uploads/2016/09/malevich-club-lviv-logo.jpg'}} />
+           <Thumbnail source={{uri: DEFAULT_VALUES.club.logoUrl }} />
            <Body>
-             <Text>Malevich club </Text>
-             <Text note>Rating: 4.5</Text>
+             <Text>{DEFAULT_VALUES.club.name}</Text>
+             <Text note>Rating: {DEFAULT_VALUES.club.rating}</Text>
            </Body>
          </Left>
          <Right>
-           <Text style={styles.date}>23 Aug</Text>
+           <Text style={styles.date}>{getDayNumber(data.startAt)} {getMonthText(data.startAt)}</Text>
            <View style={styles.day}>
-            <Text style={styles.dateDay}>monday</Text>
+            <Text style={styles.dateDay}>{getWeekDayText(data.startAt)}</Text>
           </View>
          </Right>
       </CardItem>
       <CardItem cardBody>
-        <Image source={{uri: 'https://scontent.flwo1-1.fna.fbcdn.net/v/t35.0-12/23516209_1967578123509144_294036789_o.jpg?oh=024b55ac16e78119d6f9b3b4a4b46c44&oe=5A0B9CDA'}} style={styles.bodyImage}/>
+        <Image source={{uri: DEFAULT_VALUES.event.photoUrl }} style={styles.bodyImage}/>
       </CardItem>
       <CardItem style={{flex: 1}}>
-        <Text style={styles.nameText}>MONDAY PARTY</Text>
+        <Text style={styles.nameText}>{data.name}</Text>
         <View style={{marginLeft: 'auto'}}>
           <Text style={styles.time}>
-            09
+            {getDateHour(data.startAt)}
             <Text style={styles.doubleDot}>:
-              <Text style={styles.time}>00</Text>
+              <Text style={styles.time}>{getDateMinute(data.startAt)}</Text>
             </Text>
           </Text>
         </View>
--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import { observable } from 'mobx';
+import { observer, inject } from 'mobx-react';
+import { List, Spinner } from 'native-base';
 import EventCard from './EventCard';
 
+@inject('event')
+@observer
 class EventList extends Component {
+
+  componentDidMount = async () => {
+    try {
+      const { event } = this.props;
+      this.events = await event.getList();
+      this.isLoading = false;
+    } catch (error) {
+      alert('something went wrong =(');
+    }
+  }
+
+  @observable isLoading = true;
+  @observable events = [];
+
   render() {
+
+    const renderEvents = () => {
+      const { events } = this;
+      return events.map(item => (
+        <EventCard data={item} key={item._id} />
+      ));
+    }
+
     return (
-      <EventCard />
+      <List>
+        { this.isLoading ? <Spinner /> : renderEvents() }
+      </List>
     );
   }
 }
