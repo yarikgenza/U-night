3bc23b0764a1f01e98b237200bf8361e9eb80129
Yarik Genza <gsmeter@gmail.com>
2017-12-02 00:26:21 +0200

    create layout for scan-code screen

--- src/screens/scan.screen.js
+++ src/screens/scan.screen.js
@@ -1,57 +1,104 @@
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  Dimensions,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View
-} from 'react-native';
+import { Image } from 'react-native';
+import { View, Text, Button } from 'native-base';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
 import Camera from 'react-native-camera';
+import * as Animatable from 'react-native-animatable';
 
+import NavBar from '../components/NavBar';
+
+import theme from '../theme';
+const { deviceHeight, deviceWidth } = theme;
+
+@observer
 class Scan extends Component {
+
+  @observable  showScanner = false;
+  @observable imgCode = 0;
+
   render() {
-    return (
+    const renderScanner = () => (
       <View style={styles.container}>
         <Camera
           ref={(cam) => {
             this.camera = cam;
           }}
           style={styles.preview}
-          aspect={Camera.constants.Aspect.fill}>
-          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
-        </Camera>
+          aspect={Camera.constants.Aspect.fill}
+        />
+      </View>
+    );
+
+    return this.showScanner ? renderScanner() : (
+      <View style={styles.layout}>
+        <NavBar title='Scan code' />
+        <Animatable.Image
+          style={styles.image}
+          delay={300}
+          animation="pulse"
+          iterationCount="infinite"
+          useNativeDriver
+          source={require('../images/qr-code-2.png')}
+        />
+        <View style={styles.offerTextContainer}>
+          <Text style={styles.offerText}>The best offers from partner clubs</Text>
+          <Text style={styles.offerText}>are available for you!</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button style={styles.button} block light>
+            <Text style={styles.buttonText}>Scan QR-code</Text>
+          </Button>
+        </View>
       </View>
     );
   }
-
-  takePicture() {
-    const options = {};
-    //options.location = ...
-    this.camera.capture({metadata: options})
-      .then((data) => console.log(data))
-      .catch(err => console.error(err));
-  }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
     flexDirection: 'row',
+  },
+  layout: {
+    flex: 1,
+    backgroundColor: 'black',
   },
   preview: {
     flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center'
   },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
+  image: {
+    marginTop: 50,
+    height: 180,
+    width: 180,
+    alignSelf: 'center',
+  },
+  offerTextContainer: {
+    flex: 0.45,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  offerText: {
+    color: '#8c9193',
+    fontSize: 19,
+  },
+  buttonContainer: {
+    flex: 0.5,
+    justifyContent: 'center',
+  },
+  button: {
     borderRadius: 5,
-    color: '#000',
-    padding: 10,
-    margin: 40
+    alignSelf: 'center',
+    width: deviceWidth - (deviceWidth / 4),
+    backgroundColor: '#9e4ed4',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
   }
-});
+};
 
 export default Scan;
